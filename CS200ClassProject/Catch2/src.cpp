af1cf22b41c2d05ba898ae24fca713b16633996b
Il <rtd990@naver.com>
2019-09-20 18:43:05 +0900

    catch is added and try to use it.

--- CS200ClassProject/Catch2/src.cpp
+++ CS200ClassProject/Catch2/src.cpp
@@ -1,25 +1,24 @@
-#include <vector2.hpp>
 // 010-TestCase.cpp
 
 // Let Catch provide main():
- #define CATCH_CONFIG_MAIN
-//#define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#define CATCH_CONFIG_MAIN
 
-int Factorial(int number) {
-	return number <= 1 ? number : Factorial(number - 1) * number;  // fail
- // return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+#include "catch.hpp"
+
+int Factorial( int number ) {
+   return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
+// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
 }
 
-TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]") {
-	REQUIRE(Factorial(0) == 1);
+TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 1 );
 }
 
-TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]") {
-	REQUIRE(Factorial(1) == 1);
-	REQUIRE(Factorial(2) == 2);
-	REQUIRE(Factorial(3) == 6);
-	REQUIRE(Factorial(10) == 3628800);
+TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
 }
 
 // Compile & run:
@@ -35,9 +34,3 @@ TEST_CASE("Factorials of 1 and higher are computed (pass)", "[single-file]") {
 // 010-TestCase.cpp:20: passed: Factorial(3) == 6 for: 6 == 6
 // 010-TestCase.cpp:21: passed: Factorial(10) == 3628800 for: 3628800 (0x375f00) == 3628800 (0x375f00)
 // Failed 1 test case, failed 1 assertion.
-
-//
-//int main(int argc, char* argv[])
-//{
-//	
-//}
--- CS200ClassProject/MathLib/vector2.hpp
+++ CS200ClassProject/MathLib/vector2.hpp
@@ -10,7 +10,7 @@
 namespace Graphics
 {
 	template <typename T>
-	struct [[nodiscard]] vector2
+	struct vector2
 	{
 		union
 		{
